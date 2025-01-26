0c3a5f94dd23b059d92b7d5418ecd198acf3a90e
lhs06 <S10258972@connect.np.edu.sg>
2025-01-27 00:04:57 +0800

    added 5 classes

---
+++ assg 2/Flight.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assg
+{
+    abstract class Flight
+    {
+        public string FlightNumber { get; set; }
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public DateTime ExpectedTime { get; set; }
+        public string Status { get; set; }
+
+        public Flight() { }
+
+        public Flight(string fn, string o, string des, DateTime et, string s)
+        {
+            FlightNumber = fn;
+            Origin = o;
+            Destination = des;
+            ExpectedTime = et;
+            Status = s;
+        }
+
+        public abstract double CalculateFees();
+
+        public override string ToString()
+        {
+            return $"Flight Number: {FlightNumber}, Origin: {Origin}, Destination: {Destination}, Expected Time: {ExpectedTime}, Status: {Status}";
+        }
+    }
+
+}
+
---
+++ assg 2/NORMFlight.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assg
+{
+    class NORMFlight : Flight
+    {
+        public NORMFlight(string fn, string o, string des, DateTime et, string s) : base(fn, o, des, et, s) { }
+
+        public override double CalculateFees()
+        {
+            return 500.0; // Base fee for normal flights
+        }
+
+        public override string ToString()
+        {
+            return $"NORMFlight - {base.ToString()}";
+        }
+    }
+}
