3e92d1454e9d605df077db679db0f7ce5ea4316c
advl511 <s10267479@connect.np.edu.sg>
2025-01-26 23:56:08 +0800

    Add project files.

---
+++ assg 2/Airline.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Airline
+{
+    private string name;
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    private string code;
+    public string Code
+    {
+        get { return code; }
+        set { code = value; }
+    }
+
+    private Dictionary<string, Flight> flights;
+    public Dictionary<string, Flight> Flights
+    {
+        get { return flights; }
+        set { flights = value; }
+    }
+
+    public Airline(string name, string code, Dictionary<string, Flight> flights)
+    {
+        Name = name;
+        Code = code;
+        Flights = flights;
+    }
+    public bool AddFlight(Flight)
+    {
+        Flights.Add(name, code);
+        return true;
+    }
+    public double CalculateFees()
+    {
+        double fees = 0;
+        Console.WriteLine("placeholder");
+        return fees;
+    }
+    public bool RemoveFlight(Flight)
+    {
+        Flights.Remove(name);
+        return true;
+    }
+    public override string ToString()
+    {
+        return "Airline; " + Name + "(" + Code + ")";
+    }
+
+
+}
---
+++ assg 2/Terminal.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+internal class Terminal
+{
+    internal class Terminal
+    {
+        private string terminalName;
+        public string TerminalName
+        {
+            get { return terminalName; }
+            set { terminalName = value; }
+        }
+
+        private Dictionary<string, Airline> airlines;
+        public Dictionary<string, Airline> Airlines
+        {
+            get { return airlines; }
+            set { airlines = value; }
+        }
+
+        private Dictionary<string, Flight> flights;
+        public Dictionary<string, Flight> Flights
+        {
+            get { return flights; }
+            set { flights = value; }
+        }
+
+        private Dictionary<string, BoardingGate> boardingGates;
+        public Dictionary<string, BoardingGate> BoardingGates
+        {
+            get { return boardingGates; }
+            set { boardingGates = value; }
+        }
+
+        private Dictionary<string, double> gateFees;
+        public Dictionary<string, double> GateFees
+        {
+            get { return gateFees; }
+            set { gateFees = value; }
+        }
+
+        public Terminal(string terminalName, Dictionary<string, Airline> airlines, Dictionary<string, Flight> flights, Dictionary<string, BoardingGate> boardingGates, Dictionary<string, double> gateFees)
+        {
+            TerminalName = terminalName;
+            Airlines = airlines;
+            Flights = flights;
+            BoardingGates = boardingGates;
+            GateFees = gateFees;
+        }
+
+
+        public bool AddAirline(Airline airline)
+    {
+            Airlines.Add(airline.Name, airline);
+            return true;
+    }
+    public bool AddBoardingGate(BoardingGate boardingGate)
+    {
+        BoardingGates.Add(boardingGate.Name, boardingGate);
+        return true;
+    }
+    public Airline GetAirlineFromFlight(Flight flight)
+    {
+        foreach (var airline in Airlines.Values)
+        {
+            if (airline.Flights.Contains(flight))
+            {
+                return airline;
+            }
+        }
+        return null;
+    }
+    public void PrintAirlineFees()
+    {
+        Console.WriteLine("${0}", gateFees);
+    }
+    public string ToString()
+    {
+        return string.Format(null);
+    }
+}
+
